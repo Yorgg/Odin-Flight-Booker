5261939619bb4b53b7b3bcf327a8b8eebac0d526
Jon <jyorg@outlook.com>
2015-05-20 23:55:20 -0700

    Add styling

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ActiveRecord::Base
   belongs_to :from_airport, :class_name => "Airport"
   belongs_to :to_airport, :class_name => "Airport"
+
+  scope :on_day, ->(time) { where("departure_date >= ? AND departure_date <= ?", time, time.to_date + 1.day) }
 end
